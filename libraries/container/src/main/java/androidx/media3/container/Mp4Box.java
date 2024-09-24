ef1415edd64a177c1326b9e0c16f8bd6ac5f3d57
tokii <mottchenz@gmail.com>
2024-09-24 20:02:35 +0800

    Fix ProRes Support for ap4h & ap4x.
    (cherry picked from commit a2a6d378d32ae82040fec3864beaa6ddc1daeac7)

--- libraries/container/src/main/java/androidx/media3/container/Mp4Box.java
+++ libraries/container/src/main/java/androidx/media3/container/Mp4Box.java
@@ -478,7 +478,12 @@ public abstract class Mp4Box {
 
   @SuppressWarnings({"ConstantCaseForConstants", "IdentifierName"})
   public static final int TYPE_pcmC = 0x70636d43;
-  public static final int TYPE_ap4x = 0x61703468;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_ap4x = 0x61703478;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_ap4h = 0x61703468;
 
   @SuppressWarnings("ConstantCaseForConstants")
   public static final int TYPE_apch = 0x61706368;
