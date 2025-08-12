ef627e6cde04b030c7ce2aec7f19bb88d51d956f
Peter Bennett <pbennett@mythtv.org>
2025-08-12 11:16:26 -0400

    Remove duplicate mime type vc1

--- libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/FfmpegLibrary.java
+++ libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/FfmpegLibrary.java
@@ -171,8 +171,6 @@ public final class FfmpegLibrary {
         return "mpeg2video";
       case MimeTypes.VIDEO_ProRes:
         return "prores";
-      case MimeTypes.VIDEO_VC1:
-        return "vc1";
       case MimeTypes.VIDEO_MP4V:
       case MimeTypes.VIDEO_DIVX:
         return "mpeg4";
