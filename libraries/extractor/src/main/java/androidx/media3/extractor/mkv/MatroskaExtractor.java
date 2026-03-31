eb5a927d9a097e5d1dac87c4e8da075a17dc5d59
Ian Baker <ibaker@google.com>
2026-03-31 08:13:19 +0100

    Typo fix

--- libraries/extractor/src/main/java/androidx/media3/extractor/mkv/MatroskaExtractor.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mkv/MatroskaExtractor.java
@@ -1721,7 +1721,7 @@ public class MatroskaExtractor implements Extractor {
   /**
    * Returns the chapter corresponding to the current EditionEntry element.
    *
-   * @throws ParserException if the element id is not in a EditionEntry.
+   * @throws ParserException if the element id is not in an EditionEntry.
    */
   protected ChapterEntry getCurrentChapter(int currentElementId) throws ParserException {
     assertInEditionEntry(currentElementId);
