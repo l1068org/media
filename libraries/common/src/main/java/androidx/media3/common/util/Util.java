2d4ab61e69c74796f529bf8f9cab60c68b340d4d
FongMi <b8892778@gmail.com>
2026-05-22 01:58:42 +0800

    Improve content type handling

--- libraries/common/src/main/java/androidx/media3/common/util/Util.java
+++ libraries/common/src/main/java/androidx/media3/common/util/Util.java
@@ -3050,6 +3050,21 @@ public final class Util {
       return C.CONTENT_TYPE_RTSP;
     }
 
+    if ("data".equals(scheme)) {
+      if (uri.getSchemeSpecificPart().startsWith(MimeTypes.APPLICATION_MPD)) {
+        return C.CONTENT_TYPE_DASH;
+      } else {
+        return C.CONTENT_TYPE_HLS;
+      }
+    }
+
+    String url = uri.toString();
+    if (url.contains("=m3u8")) {
+      return C.CONTENT_TYPE_HLS;
+    } else if (url.contains("=mpd")) {
+      return C.CONTENT_TYPE_DASH;
+    }
+
     @Nullable String lastPathSegment = uri.getLastPathSegment();
     if (lastPathSegment == null) {
       return C.CONTENT_TYPE_OTHER;
@@ -3064,6 +3079,12 @@ public final class Util {
         // disambiguate between Smooth Streaming, HLS and DASH below - so we can just return TYPE_SS
         // here without further checks.
         return contentType;
+      }
+    } else {
+      if ("m3u8".equals(lastPathSegment)) {
+        return C.CONTENT_TYPE_HLS;
+      } else if ("mpd".equals(lastPathSegment)) {
+        return C.CONTENT_TYPE_DASH;
       }
     }
 
@@ -3104,6 +3125,7 @@ public final class Util {
     switch (fileExtension) {
       case "mpd":
         return C.CONTENT_TYPE_DASH;
+      case "php":
       case "m3u8":
         return C.CONTENT_TYPE_HLS;
       case "ism":
