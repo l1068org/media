f972e344d5ee9bf5bfc238966659ea0bacc0c9fd
FongMi <b8892778@gmail.com>
2026-06-08 18:37:37 +0800

    Add media chapter and edition APIs

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/SimpleExoPlayer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/SimpleExoPlayer.java
@@ -32,6 +32,8 @@ import androidx.media3.common.C;
 import androidx.media3.common.DeviceInfo;
 import androidx.media3.common.Effect;
 import androidx.media3.common.Format;
+import androidx.media3.common.MediaChapter;
+import androidx.media3.common.MediaEdition;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.MediaMetadata;
 import androidx.media3.common.PlaybackParameters;
@@ -445,6 +447,18 @@ public class SimpleExoPlayer extends BasePlayer implements ExoPlayer {
   }
 
   @Override
+  public boolean selectChapter(MediaChapter chapter) {
+    blockUntilConstructorFinished();
+    return player.selectChapter(chapter);
+  }
+
+  @Override
+  public boolean selectEdition(MediaEdition edition) {
+    blockUntilConstructorFinished();
+    return player.selectEdition(edition);
+  }
+
+  @Override
   public void setVideoScalingMode(@C.VideoScalingMode int videoScalingMode) {
     blockUntilConstructorFinished();
     player.setVideoScalingMode(videoScalingMode);
@@ -734,6 +748,7 @@ public class SimpleExoPlayer extends BasePlayer implements ExoPlayer {
     blockUntilConstructorFinished();
     return player.getAudioFormat();
   }
+
 
   @Override
   @Nullable
--- libraries/session/src/main/java/androidx/media3/session/MediaControllerImplLegacy.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaControllerImplLegacy.java
@@ -41,6 +41,8 @@ import androidx.annotation.Nullable;
 import androidx.media3.common.AudioAttributes;
 import androidx.media3.common.C;
 import androidx.media3.common.DeviceInfo;
+import androidx.media3.common.MediaChapter;
+import androidx.media3.common.MediaEdition;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.MediaMetadata;
 import androidx.media3.common.PlaybackException;
@@ -1367,6 +1369,16 @@ import org.checkerframework.checker.initialization.qual.UnderInitialization;
   }
 
   @Override
+  public List<MediaChapter> getCurrentMediaChapters() {
+    return ImmutableList.of();
+  }
+
+  @Override
+  public List<MediaEdition> getCurrentMediaEditions() {
+    return ImmutableList.of();
+  }
+
+  @Override
   public TrackSelectionParameters getTrackSelectionParameters() {
     return TrackSelectionParameters.DEFAULT;
   }
@@ -2464,6 +2476,8 @@ import org.checkerframework.checker.initialization.qual.UnderInitialization;
             seekForwardIncrementMs,
             maxSeekToPreviousPositionMs,
             /* currentTracks= */ Tracks.EMPTY,
+            /* currentMediaChapters= */ ImmutableList.of(),
+            /* currentMediaEditions= */ ImmutableList.of(),
             /* parameters= */ TrackSelectionParameters.DEFAULT);
 
     return new ControllerInfo(
