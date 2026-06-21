94c1839c489a706d3c78d5b22a035c8451006afb
FongMi <b8892778@gmail.com>
2026-06-22 07:05:10 +0800

    Sort Matroska chapters by start time

--- libraries/extractor/src/main/java/androidx/media3/extractor/mkv/MatroskaExtractor.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mkv/MatroskaExtractor.java
@@ -2938,6 +2938,7 @@ public class MatroskaExtractor implements Extractor {
         }
       }
       if (!chapters.isEmpty()) {
+        chapters.sort((first, second) -> Long.compare(first.getStartTimeMs(), second.getStartTimeMs()));
         format =
             checkNotNull(format)
                 .buildUpon()
